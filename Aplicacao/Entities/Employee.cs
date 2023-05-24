ba9b417b5bffdf5e6795e83a88cd4b8a6bf01643
Luiz Fernando <conttatoluiz@gmail.com>
2023-05-24 00:13:06 -0300

    Adicionar arquivos de projeto.

---
+++ Aplicacao/Entities/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Aplicacao.Entities
+{
+    class Employee
+    {
+
+        public String Name { get; set; }
+        public String Email { get; set; }
+        public Double Salary { get; set; }
+
+        public Employee(string name, string email, double salary)
+        {
+            Name = name;
+            Email = email;
+            Salary = salary;
+        }
+    }
+}
